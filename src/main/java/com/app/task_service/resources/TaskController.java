e491cc478b6dbf432e630927471cf1f6de4a2469
Abdallah Samir <abdallah01097261247@gmail.com>
2026-08-18 21:31:19 +0300

    Adding new code to trigger pipeline

--- src/main/java/com/app/task_service/resources/TaskController.java
+++ src/main/java/com/app/task_service/resources/TaskController.java
@@ -2,6 +2,7 @@ package com.app.task_service.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class TaskController {
         return ResponseEntity.ok(
                 List.of("Write feature", "write tests", "generate reports")
         );
+    }
+
+    @PostMapping
+    public void add(String task){
+        System.out.printf("Task %s has been added", task);
     }
 }
